720601e746a867d47721bed0a134d870d08fa454
MLpzs <marcolopes1928@gmail.com>
2025-07-06 03:22:45 -0300

    Ajusta app para usar porta do Railway

--- app.py
+++ app.py
@@ -31,7 +31,9 @@ def index():
                              download_name="convertido.png",
                              mimetype='image/png')
     return render_template_string(HTML)
+import os
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
